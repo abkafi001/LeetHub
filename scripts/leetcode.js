cedfc1b7d2bb5d2b17af6232c93cdd67751a89c3
Abdullahil Kafi <abkafi001@gmail.com>
2022-10-20 01:47:47 +0600

    codition added to parseQuestion()
    New parseQuestion() function works only for OLD UI and explore section and returns null otherwise. Because for new UI we leverage chrome's local storage.

--- scripts/leetcode.js
+++ scripts/leetcode.js
@@ -483,59 +483,64 @@ function addLeadingZeros(title) {
 
 /* Parser function for the question and tags */
 function parseQuestion() {
-  var questionUrl = window.location.href;
-  if (questionUrl.endsWith('/submissions/')) {
-    questionUrl = questionUrl.substring(
-      0,
-      questionUrl.lastIndexOf('/submissions/') + 1,
+
+  // old code: applies to both old UI and explore section
+  if(OLD_UI || window.location.pathname.startsWith('/explore')) {
+
+    var questionUrl = window.location.href;
+    if (questionUrl.endsWith('/submissions/')) {
+      questionUrl = questionUrl.substring(
+        0,
+        questionUrl.lastIndexOf('/submissions/') + 1,
+      );
+    }
+    const questionElem = document.getElementsByClassName(
+      'content__u3I1 question-content__JfgR',
     );
-  }
-  const questionElem = document.getElementsByClassName(
-    'content__u3I1 question-content__JfgR',
-  );
-  const questionDescriptionElem = document.getElementsByClassName(
-    'question-description__3U1T',
-  );
-  if (checkElem(questionElem)) {
-    const qbody = questionElem[0].innerHTML;
-
-    // Problem title.
-    let qtitle = document.getElementsByClassName('css-v3d350');
-    if (checkElem(qtitle)) {
-      qtitle = qtitle[0].innerHTML;
-    } else {
-      qtitle = 'unknown-problem';
-    }
-
-    // Problem difficulty, each problem difficulty has its own class.
-    const isHard = document.getElementsByClassName('css-t42afm');
-    const isMedium = document.getElementsByClassName('css-dcmtd5');
-    const isEasy = document.getElementsByClassName('css-14oi08n');
-
-    if (checkElem(isEasy)) {
-      difficulty = 'Easy';
-    } else if (checkElem(isMedium)) {
-      difficulty = 'Medium';
-    } else if (checkElem(isHard)) {
-      difficulty = 'Hard';
-    }
-    // Final formatting of the contents of the README for each problem
-    const markdown = `<h2><a href="${questionUrl}">${qtitle}</a></h2><h3>${difficulty}</h3><hr>${qbody}`;
-    return markdown;
-  } else if (checkElem(questionDescriptionElem)) {
-    let questionTitle = document.getElementsByClassName(
-      'question-title',
+    const questionDescriptionElem = document.getElementsByClassName(
+      'question-description__3U1T',
     );
-    if (checkElem(questionTitle)) {
-      questionTitle = questionTitle[0].innerText;
-    } else {
-      questionTitle = 'unknown-problem';
+    if (checkElem(questionElem)) {
+      const qbody = questionElem[0].innerHTML;
+
+      // Problem title.
+      let qtitle = document.getElementsByClassName('css-v3d350');
+      if (checkElem(qtitle)) {
+        qtitle = qtitle[0].innerHTML;
+      } else {
+        qtitle = 'unknown-problem';
+      }
+
+      // Problem difficulty, each problem difficulty has its own class.
+      const isHard = document.getElementsByClassName('css-t42afm');
+      const isMedium = document.getElementsByClassName('css-dcmtd5');
+      const isEasy = document.getElementsByClassName('css-14oi08n');
+
+      if (checkElem(isEasy)) {
+        difficulty = 'Easy';
+      } else if (checkElem(isMedium)) {
+        difficulty = 'Medium';
+      } else if (checkElem(isHard)) {
+        difficulty = 'Hard';
+      }
+      // Final formatting of the contents of the README for each problem
+      const markdown = `<h2><a href="${questionUrl}">${qtitle}</a></h2><h3>${difficulty}</h3><hr>${qbody}`;
+      return markdown;
+    } else if (checkElem(questionDescriptionElem)) {
+      let questionTitle = document.getElementsByClassName(
+        'question-title',
+      );
+      if (checkElem(questionTitle)) {
+        questionTitle = questionTitle[0].innerText;
+      } else {
+        questionTitle = 'unknown-problem';
+      }
+
+      const questionBody = questionDescriptionElem[0].innerHTML;
+      const markdown = `<h2>${questionTitle}</h2><hr>${questionBody}`;
+
+      return markdown;
     }
-
-    const questionBody = questionDescriptionElem[0].innerHTML;
-    const markdown = `<h2>${questionTitle}</h2><hr>${questionBody}`;
-
-    return markdown;
   }
 
   return null;
